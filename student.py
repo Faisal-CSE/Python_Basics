da3451d712075fbf574e265fa16f82af67408235
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2021-05-07 03:18:11 +0600

    2nd commit

--- student.py
+++ student.py
@@ -5,3 +5,10 @@ class Student:
         self.dept = dept
         self.cgpa = cgpa
         self.current_semester = current_semester
+
+    def is_honor_roll(self):
+        if self.cgpa >= 3.5:
+            return True
+        else:
+            return False
+
